6713a3b88dc11241cc0e52c9c9ed59acd50a5bd2
olgamikhalova <olya_mikhalyova@yahoo.com>
2018-03-30 17:12:59 +0300

    Change task#8

--- homework#2/8/8.js
+++ homework#2/8/8.js
@@ -1,16 +1,23 @@
-function myFunction() {	
+function makeTable() {	
 	var sizeOfBoard = document.getElementById("size").value;
 	var arr = sizeOfBoard.split("*");
-	
-	document.write("<table border='1' width='400' height='400'>");
-    for (var i = 1; i <= arr[1]; i++){
-		document.write("<tr>");
-		for (var j = 1; j <= arr[0]; j++){
-			if((i + j) % 2 != 0){
-				document.write("<td bgcolor='black'></td>");
+	var table = document.createElement('TABLE');
+	document.body.appendChild(table);
+	var tr = document.createElement('TR');
+	var td = document.createElement('TD');
+	td.style.width = '50px';
+	td.style.height = '50px';
+	for(var i = 0; i < arr[1]; i++){
+		table.appendChild(tr.cloneNode(true));
+		for(var j = 0; j < arr[0]; j++){
+			var row = table.getElementsByTagName('TR')[i];
+			row.appendChild(td.cloneNode(true));
+			var data = table.getElementsByTagName('TR')[i].getElementsByTagName('TD')[j];
+			if((i % 2 == 0 && j % 2 == 0) || i % 2 == 1 && j % 2 == 1){
+				data.style.backgroundColor = '#994d00';
 			}else{
-				document.write("<td bgcolor='white'></td>");
+				data.style.backgroundColor = '#ffe066';
 			}
 		}
-    }
+	}
 }
