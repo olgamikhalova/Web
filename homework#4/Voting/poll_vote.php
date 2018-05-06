8df4e451fbe3a4086098159509e1ba0fe52cedce
olgamikhalova <olya_mikhalyova@yahoo.com>
2018-05-06 20:35:20 +0300

    change 'get' for 'post', delete ajax, add submit button

--- homework#4/Voting/poll_vote.php
+++ homework#4/Voting/poll_vote.php
@@ -10,10 +10,13 @@ $vote = $_REQUEST['vote'];
 $filename = "poll_result.json";
 
 $content = json_decode(file_get_contents($filename), true)?: ['Tea' => 0, 'Coffee' => 0];
-if ($vote == 0) $content['Tea'] += 1;
-if ($vote == 1) $content['Coffee'] += 1;
+if ($vote == 'tea_drink') $content['Tea'] += 1;
+if ($vote == 'coffee_drink') $content['Coffee'] += 1;
 
 file_put_contents($filename, json_encode($content, JSON_PRETTY_PRINT));
 ?>
+<script type="text/javascript">
+	location.href = "statistics.html";								
+</script>
 </body>
 </html>
