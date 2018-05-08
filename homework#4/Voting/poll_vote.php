56aa0d0e29371c412e8ccc1da82566a2a456fe12
olgamikhalova <olya_mikhalyova@yahoo.com>
2018-05-08 23:15:42 +0300

    remove script from head to body, location.href change for header()

--- homework#4/Voting/poll_vote.php
+++ homework#4/Voting/poll_vote.php
@@ -2,12 +2,10 @@
 $vote = $_REQUEST['vote'];
 $filename = "poll_result.json";
 
-$content = json_decode(file_get_contents($filename), true) ?: ['Tea' => 0, 'Coffee' => 0];
+$content = json_decode(file_get_contents($filename), true);
 
 isset($content[$vote]) ? $content[$vote]++ : $content[$vote] = 1;
 
 file_put_contents($filename, json_encode($content, JSON_PRETTY_PRINT));
+header('Location: statistics.html');
 ?>
-<script type="text/javascript">
-	location.href = "statistics.html";								
-</script>
